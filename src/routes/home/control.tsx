78f74904b660adeb84a4a947a5757441e0672963
jinvicky <jinvicky17@gmail.com>
2022-01-07 17:34:44 +0900

    이미지 축소 기능 구현중

--- src/routes/home/control.tsx
+++ src/routes/home/control.tsx
@@ -1,5 +1,5 @@
 import { Fragment, FunctionalComponent, h } from 'preact';
-import { useState } from 'preact/hooks';
+import { useEffect, useState } from 'preact/hooks';
 import style from './style.scss';
 
 const Home: FunctionalComponent = () => {
@@ -24,12 +24,23 @@ const Home: FunctionalComponent = () => {
     //DESC:: rotate 여부 체크
     const [rotate, setRotate] = useState(false);
 
+    //DESC:: resize 여부 체크
+    const [resize, setResize] = useState(false)
+
     //DESC:: 회전 시 각도를 저장
     const [deg, setDeg] = useState(0);
 
+    //DESC::  이미지 크기를 저장
+    const [imgSize, setImgSize] = useState({ width: 0, height: 0 });
+
+
+    //DESX:: 테스트 겸 control 버튼의 시작점 저장하기. 
+    const [startPos, setStartPos] = useState(0);
+
+
     //DESC:: 이미지를 이동시키는 함수
     const movePosOfBox = (e: MouseEvent) => {
-        const j = document.getElementById("moveSP"); //.baseLayout
+        const j = document.getElementById("moveSP"); //.moveableSpace
         const jin = j?.getBoundingClientRect();
 
         if (jin !== undefined)
@@ -38,14 +49,8 @@ const Home: FunctionalComponent = () => {
                 left: e.clientX - cursor.x - jin?.left
             });
     }
-
-    //DESC:: 공간에서 mouseDown했을 때 발생 함수.
-    const spaceMouseD = () => {
-        setRotate(true);
-    }
-
-    //DESC:: 공간에서 mouseMove할때 발생 함수.
-    const spaceMouseM = (e: MouseEvent) => {
+    //DESC:: 이미지 회전하는 함수.
+    const rotateBox = (e: MouseEvent) => {
         const image = document.getElementById("image");
 
         if (image) {
@@ -65,9 +70,60 @@ const Home: FunctionalComponent = () => {
         }
     }
 
+    //DESC:: 공간에서 mouseDown했을 때 발생 함수.
+    const spaceMouseD = () => {
+        setRotate(true);
+    }
+    //DESC:: 공간에서 mouseMove할때 발생 함수.
+    const spaceMouseM = (e: MouseEvent) => {
+
+        //1. 이미지 박스 회전하기.
+        rotateBox(e);
+
+        const cRef = document.getElementById("toTop"); //함수를 만들 때 toTop등을 파람으로 받아오자.
+        if (cRef) {
+
+
+            let diffPos = startPos - e.clientY;
+
+            // diffPos = e.clientY - startPos;
+
+            console.log("1. ", imgSize.height);
+            console.log("2. ", diffPos);
+            console.log("3. ", imgSize.height - diffPos);
+
+            if (resize)
+                setImgSize({ ...imgSize, height: imgSize.height - diffPos });
+
+        }
+
+    }
+    //DESC:: control의 시작점을 정하는 함수.
+    const toDownControlMouseD = (e: MouseEvent) => {
+        setResize(true);
+        setStartPos(e.clientY);
+
+    }
     const spaceMouseU = () => {
         setRotate(false);
+        setResize(false);
     }
+    //DESC:: control버튼의 onMouseMove 
+    const controlMouseM = () => {
+
+    }
+    //DESC:: control버튼의 onMouseUp
+    const toDownControlMouseU = (e: MouseEvent) => {
+        // setResize(false);
+
+    }
+
+
+    useEffect(() => { // 이미지를 바꿨을 경우 기존의 변화들을 초기화 
+        setDeg(0);
+        setBoxPos({ top: 400, left: 480 });
+        setImgSize({ width: 0, height: 0 });
+    }, [file]);
 
     return <Fragment>
         <div class={style.root}>
@@ -86,7 +142,11 @@ const Home: FunctionalComponent = () => {
                 {file &&
                     <div class={style.moveableBox}
                         style={{
-                            transform: `translate(${boxPos.left}px, ${boxPos.top}px) rotate(${deg}deg)`
+                            width: imgSize.width ? imgSize.width : "auto",
+                            height: imgSize.width ? imgSize.height : "auto", // targetLine의 길이 고려
+                            transform: `translate(${boxPos.left}px, ${boxPos.top}px) rotate(${deg}deg)`,
+                            maxHeight: 250,
+                            maxWidth: 450,
                         }}
                     >
                         <div class={style.targetLine}>
@@ -100,7 +160,8 @@ const Home: FunctionalComponent = () => {
                             <div id="control"
                                 class={[style.controlBtn, style.moveSE].join(" ")}
                             />
-                            <div id="control"
+                            {/* 아래로 마우스 이동해서 이미지 줄이는 버튼  */}
+                            <div id="toDown"
                                 class={[style.controlBtn, style.moveS].join(" ")}
                             />
                             <div id="control"
@@ -111,8 +172,14 @@ const Home: FunctionalComponent = () => {
                             <div id="control"
                                 class={[style.controlBtn, style.moveSW].join(" ")}
                             />
-                            <div id="control"
+                            {/* 위로 마우스 이동해서 이미지 줄이는 버튼  */}
+                            <div id="toTop"
                                 class={[style.controlBtn, style.moveS].join(" ")}
+                                onMouseDown={(e) => {
+                                    toDownControlMouseD(e);
+                                }}
+                                onMouseUp={(e) => toDownControlMouseU(e)}
+
                             />
                             <div id="control"
                                 class={[style.controlBtn, style.moveSE].join(" ")}
@@ -129,6 +196,18 @@ const Home: FunctionalComponent = () => {
                                 setMouseD(true);
                             }}
                             onMouseUp={() => setMouseD(false)}
+                            onLoad={(e) => {
+                                setImgSize({ width: e.currentTarget.width, height: e.currentTarget.height });
+                            }}
+                            style={{
+                                display: "block",
+                                objectFit: "fill",
+                                width: `100%`,
+                                height: "100%",
+                                // width: "auto",
+                                // height: "auto",
+                            }}
+
                         />
                     </div>
                 }
