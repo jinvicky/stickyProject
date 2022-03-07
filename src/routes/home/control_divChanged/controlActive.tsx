a7b07d28025d1f7c594ee8b31f412400494d6e19
jinvicky <jinvicky17@gmail.com>
2022-03-07 16:57:33 +0900

    이미지 업로드 시 비율 설정 구현

--- src/routes/home/control_divChanged/controlActive.tsx
+++ src/routes/home/control_divChanged/controlActive.tsx
@@ -31,12 +31,11 @@ const Home: FunctionalComponent = () => {
     const saveFileImg = (files: FileList) => files && setFile(URL.createObjectURL(files[0]));
 
     const [drag, setDrag] = useState(false);
-
-
     const [boxPos, setBoxPos] = useState({ top: 200, left: 200 });
 
     useEffect(() => {
         console.log("mounted");
+        setCenterOfBox();
         window.addEventListener("click", checkETarget);
         return () => {
             window.removeEventListener("click", checkETarget);
@@ -44,20 +43,13 @@ const Home: FunctionalComponent = () => {
         }
     }, []);
 
+    useEffect(() => {
+        setBoxPos({ left: 100, top: 100 });
+    }, [file]);
+
 
     const checkETarget = useCallback((e: MouseEvent) => {
 
-        const cBox = document.getElementById("controllerBox");
-        if (cBox && !cBox?.contains(e.target as HTMLElement)) {
-
-            if (drag || rotate || resize) {
-                console.log(100);
-                return;
-            }
-            else {
-                // setCActive(false);
-            }
-        }
     }, []);
 
     const boxMouseDown = useCallback((e: MouseEvent) => {
@@ -67,16 +59,6 @@ const Home: FunctionalComponent = () => {
         window.addEventListener("mousemove", movePosOfBox);
         window.addEventListener("mouseup", boxMouseUp);
     }, []);
-
-    const canvasMouseupCheck = () => {
-        /**
-         * 1. drag, rotate, resize 상태를 확인한다. 
-         * 셋 중에 하나라도 true인 상태라면 setCActive(true);
-         * 안됨ㅋㅋㅋㅋㅋㅋ
-         * controller 위치 안 맞는 거는 useEffect에서 잘못한 거 같은데. 
-         */
-        // if (drag || rotate || resize) setCActive(true);
-    }
 
     const movePosOfBox = useCallback((e: MouseEvent) => {
 
@@ -101,7 +83,6 @@ const Home: FunctionalComponent = () => {
 
     const [cActive, setCActive] = useState(false);
 
-    //DESC:: 이미지의 offset을 지정하는 함수 
     const setImgOffset = (e: MouseEvent) => {
         const i = document.getElementById("cursorHelper");
         if (i) {
@@ -110,10 +91,6 @@ const Home: FunctionalComponent = () => {
             imgOffset.y = Math.round(e.clientY - img.top);
         }
     }
-
-
-
-
 
     const [rotate, setRotate] = useState(false);
     const [degree, setDegree] = useState(0);
@@ -129,27 +106,7 @@ const Home: FunctionalComponent = () => {
             };
             setCenter(center);
         }
-        const point = document.getElementById("centerPoint");
-        if (point) {
-            const center = {
-                x: point?.getBoundingClientRect().left + point?.getBoundingClientRect().width,
-                y: point?.getBoundingClientRect().top + point?.getBoundingClientRect().height,
-            };
-            setCenter(center);
-        }
     };
-
-    useEffect(() => {
-        setCenterOfBox();
-        const c = document.getElementById("canvas");
-        if (c) {
-            const cRect = c.getBoundingClientRect();
-            /**
-             * TODO:: 화면 뷰포트 크기가 변함에 따라 tPos를 갱신하기. 
-             * (여기서 기능은 수행되지 않음)
-             */
-        }
-    }, []);
 
     const rotateBox = (e: MouseEvent) => {
         const x = e.clientX - center.x;
@@ -158,36 +115,26 @@ const Home: FunctionalComponent = () => {
         rotate && setDegree(Math.round(degree));
     };
 
-    //resize 여부 
     const [resize, setResize] = useState(false);
 
-    const checkDirection = (e: MouseEvent) => {
-
-        const direction = (e.currentTarget as Element).getAttribute("data-id");
-        if (direction) {
-            if (direction === "e" || direction === "w")
-                dir = "width";
-            else if (direction === "s" || direction === "n")
-                dir = "height";
-            else dir = "edge";
-        }
+    const checkDirection = (direction: string) => {
+        if (direction === "e" || direction === "w") dir = "width";
+        else if (direction === "s" || direction === "n") dir = "height";
+        else dir = "edge";
     };
 
-    const checkNW = (e: MouseEvent) => {
-        const direction = (e.currentTarget as Element).getAttribute("data-id");
-        if (direction) {
-
-            if (direction.indexOf("n") > -1)
-                rh = -1;
-            if (direction.indexOf("w") > -1)
-                rw = -1;
-        }
-    }
+    const checkNW = (direction: string) => {
+        if (direction.indexOf("n") > -1) rh = -1;
+        if (direction.indexOf("w") > -1) rw = -1;
+    };
 
     const controllerSetting = (e: MouseEvent) => {
+        const direction = (e.currentTarget as Element).getAttribute("data-id");
 
-        checkDirection(e);
-        checkNW(e);
+        if (direction) {
+            checkDirection(direction);
+            checkNW(direction);
+        }
 
         mousePress = { x: e.clientX, y: e.clientY };
 
@@ -202,7 +149,6 @@ const Home: FunctionalComponent = () => {
             initRotate = degree;
         }
         setResize(true);
-
     };
 
     //resize 종료 시 rx, rw, rh 값을 초기화. ry는 1로 변하지 않더라.
@@ -242,20 +188,13 @@ const Home: FunctionalComponent = () => {
         b.style.width = newW + "px";
         b.style.height = newH + "px";
 
-
-        //테스트 삼아서 #fakeControl의 width, height도 같이 변경해본다. 
-        const f = document.getElementById("controllerBox");
-        if (f) {
-            f.style.width = newW + "px";
-            f.style.height = newH + "px";
+        const c = document.getElementById("controllerBox");
+        if (c) {
+            c.style.width = newW + "px";
+            c.style.height = newH + "px";
         }
 
         setBoxPos({ left: newX, top: newY });
-
-        const canvas = document.getElementById("canvas");
-        if (canvas) {
-            const vasRect = canvas.getBoundingClientRect();
-        }
         setCenterOfBox();
     };
 
@@ -291,7 +230,6 @@ const Home: FunctionalComponent = () => {
             }
             if (b) repositBox(b);
         }
-
     };
 
     const controlArray = ["e", "w", "s", "n", "se", "ne", "sw", "nw"];
@@ -304,6 +242,33 @@ const Home: FunctionalComponent = () => {
         />;
     });
 
+    const setStickerRatio = (e: h.JSX.TargetedEvent<HTMLImageElement>) => {
+
+        const img = e.currentTarget as HTMLImageElement;
+
+        let sW = 500;
+        let sH = 281.5;
+
+        let iW = 0;
+        let iH = 0;
+
+        const b = document.getElementById("moveableBox");
+        const c = document.getElementById("controllerBox");
+        if (b && c) {
+            if (img.naturalWidth > sW) {
+                iW = sW;
+                iH = sW * img.naturalHeight / img.naturalWidth;
+            }
+            if (img.naturalHeight > sH) {
+                iW = sH * img.naturalWidth / img.naturalHeight;
+                iH = sH;
+            }
+            b.style.width = iW + "px";
+            b.style.height = iH + "px";
+            c.style.width = iW + "px";
+            c.style.height = iH + "px";
+        }
+    };
 
     return <Fragment>
         <div class={style.root}
@@ -323,7 +288,7 @@ const Home: FunctionalComponent = () => {
             }}
         >
             <div
-                class={style.moveableSpace}
+                class={style.screen}
             >
                 <div
                     id="fakeWrapper"
@@ -335,10 +300,9 @@ const Home: FunctionalComponent = () => {
                         style={{
                             left: boxPos.left,
                             top: boxPos.top,
-                            // width: 300,
-                            // height: 300,
                             transform: `translate(-50%, -50%) rotate(${degree}deg)`,
-                            display: cActive === true ? "block" : "none",
+                            // display: cActive === true ? "block" : "none",
+                            // display: block,
                         }}
                     >
                         <div class={style.targetLine}>
@@ -376,81 +340,62 @@ const Home: FunctionalComponent = () => {
                         if (cActive) setCActive(!cActive);
                     }}
                 >
-                    {file &&
-                        <>
+                    {file && <>
+                        <div
+                            id="cursorHelper"
+                            class={style.cursorHelper}
+                            style={{
+                                left: boxPos.left,
+                                top: boxPos.top,
+                            }}
+                        />
+                        <div id="boxWrapper"
+                            class={style.moveableBoxWrapper}
+                        >
                             <div
-                                id="cursorHelper"
-                                class={style.cursorHelper}
+                                id="moveableBox"
+                                class={style.stickerBox}
                                 style={{
                                     left: boxPos.left,
                                     top: boxPos.top,
+                                    transform: `translate(-50%, -50%) rotate(${degree}deg)`,
                                 }}
-                            ></div>
-                            <div id="boxWrapper"
-                                class={style.moveableBoxWrapper}
                             >
-                                <div
-                                    id="moveableBox"
-                                    class={style.moveableBox}
-                                    style={{
-                                        left: boxPos.left,
-                                        top: boxPos.top,
-                                        transform: `translate(-50%, -50%) rotate(${degree}deg)`,
+                                <img
+                                    id="img"
+                                    class={style.sticker}
+                                    draggable={false}
+                                    // src="https://i.ytimg.com/vi/JVzwiFKfLEU/hq720_live.jpg?sqp=CMDd4JAG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLC98d0XVx3vMyvPP_CHzugjSeiGkQ"
+                                    src={file}
+                                    onLoad={(e) => setStickerRatio(e)}
+                                    onMouseDown={(e) => {
+                                        if (!cActive) boxMouseDown(e)
                                     }}
-                                >
-                                    <div
-                                        id="centerPoint"
-                                        class={style.centerPoint}
-                                    />
-                                    <img
-                                        id="img"
-                                        class={style.uploadImg}
-                                        draggable={false}
-                                        // src="https://i.ytimg.com/vi/JVzwiFKfLEU/hq720_live.jpg?sqp=CMDd4JAG-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&amp;rs=AOn4CLC98d0XVx3vMyvPP_CHzugjSeiGkQ"
-                                        src={file}
-                                        onLoad={(e) => {
-                                            const i = e.target as HTMLImageElement;
-
-                                            const cont = document.getElementById("controllerBox");
-                                            if (cont) {
-                                                cont.style.width = i.width + "px";
-                                                cont.style.height = i.height + "px";
-                                            }
-                                            setCActive(true);
-                                        }}
-                                        onMouseDown={(e) => {
-                                            if (!cActive) boxMouseDown(e)
-                                        }}
-                                        onMouseUp={(e) => {
-                                            if (!cActive) {
-                                                boxMouseUp(e);
-                                                // setCActive(true);
-                                            }
-                                        }}
-                                        onClick={(e) => {
-                                            e.stopPropagation(); //canvas 방지
+                                    onMouseUp={(e) => {
+                                        if (!cActive) {
+                                            boxMouseUp(e);
                                             // setCActive(true);
-                                        }}
-
-
-                                    />
-                                </div>
+                                        }
+                                    }}
+                                    onClick={(e) => {
+                                        e.stopPropagation(); //canvas 방지
+                                        // setCActive(true);
+                                    }}
+                                />
                             </div>
-                        </>
+                        </div>
+                    </>
                     }
                 </div>
             </div>
             <input type="file"
-                hidden
                 id="upload"
+                class={style.uploadBtn}
                 onInput={(e) => {
                     const target = e.target as HTMLInputElement;
                     if (target.files && target.files.length > 0) saveFileImg(target.files);
                 }}
             />
-            <label htmlFor="upload"
-                class={[style.imageBtn, file && style.active].join(" ")}
-            >이미지</label>
         </div >
     </Fragment >
 };
